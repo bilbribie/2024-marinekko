aa480892cbadbc8709bc8b0931cc862c6068ff64
bilbribie <142487020+bilbribie@users.noreply.github.com>
2024-03-02 19:18:38 +0700

    2/3

--- marinekko/src/App.js
+++ marinekko/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hiiiii</h1>
+      <h2>hello</h2>
     </div>
   );
 }
---
+++ marinekko/src/components/navbar/nav1.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function nav1() {
+  return (
+    <nav>
+      {/* Navigation links go here */}
+    </nav>
+  );
+}
+
+export default nav1;
